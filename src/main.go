65be35eb99598e660e464dfe4e406e3f0082a715
Samour <aaron.burke.cpp@gmail.com>
2024-06-09 02:51:18 +1000

    Serve page from HTML resource file

--- src/main.go
+++ src/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handleRoot)
+	fmt.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! This is Go")
+	t, _ := template.ParseFiles("resources/pages/home.html")
+	t.Execute(w, nil)
 }
