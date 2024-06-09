e6da6b4f9a370bf5348fa9bfe2e5f2caa8b5c9e7
Samour <aaron.burke.cpp@gmail.com>
2024-06-09 16:19:04 +1000

    Add custom error page

--- src/controllers/errorpages.go
+++ src/controllers/errorpages.go
@@ -3,5 +3,9 @@ package controllers
 import "net/http"
 
 func ErrorPage(w http.ResponseWriter, errorMsg string, httpCode int) {
-	http.Error(w, errorMsg, httpCode)
+	w.WriteHeader(httpCode)
+	err := Templates.ExecuteTemplate(w, "error.html", errorMsg)
+	if err != nil {
+		http.Error(w, errorMsg, httpCode)
+	}
 }
--- src/controllers/templates.go
+++ src/controllers/templates.go
@@ -7,5 +7,6 @@ var Templates = template.Must(template.ParseFiles(
 	"../resources/components/page_header.html",
 
 	"../resources/pages/edit_poll.html",
+	"../resources/pages/error.html",
 	"../resources/pages/home.html",
 ))
